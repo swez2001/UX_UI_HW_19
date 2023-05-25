7f3505560f7bef797ab2e0d374b8985cd78a2c4a
swez2001 <swethagollapalli01@gmail.com>
2023-05-25 18:59:54 +1000

    Outline for scroll function
    Fixing bug

--- js/index.js
+++ js/index.js
@@ -8,4 +8,18 @@ $(document).ready(function() {
         $(this).find('.dropdown').hide();
       }
     );
+
+    $('.scrollimg').click(function() {
+      var target = $('.section.active').scrollimg('.section');
+      if (target.length == 0){
+        target = $('.section:first');
+      }
+      $('html, body').animate({scrollTop: target.offset().top}, 'slow');
+      $('.active').removeClass('active');
+      target.addClass('active');
+   
+    });
   });
+
+  
+  
