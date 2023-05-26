50731a764856f74aae45fca4632fcbc681050f5b
swez2001 <swethagollapalli01@gmail.com>
2023-05-26 18:30:49 +1000

    Javascript changes to scroll function & work nav menu display

--- js/index.js
+++ js/index.js
@@ -1,24 +1,50 @@
 
 $(document).ready(function() {
-    $('.Work').hover(
-      function() {
-        $(this).find('.dropdown').show();
-      },
-      function() {
-        $(this).find('.dropdown').hide();
-      }
-    );
+    // $('.Work').hover(
+    //   function() {
+    //     $(this).find('.dropdown').show();
+    //   },
+    //   function() {
+    //     $(this).find('.dropdown').hide();
+    //   }
+    // );
 
-    $('.scrollimg').click(function() {
-      var target = $('.section.active').scrollimg('.section');
-      if (target.length == 0){
-        target = $('.section:first');
+    $('.Work').click(function(event) {
+      event.preventDefault();
+      if ($(this).find('.dropdown').is(":visible")){
+        $(this).find('.dropdown').hide();
+      
+      }else{
+        $(this).find('.dropdown').show();
       }
-      $('html, body').animate({scrollTop: target.offset().top}, 'slow');
-      $('.active').removeClass('active');
-      target.addClass('active');
+    });
+    
+    $('.scroll-down').click(function(event) {
+      event.preventDefault();
+      //console.log("click the button");
+      const myWork = document.getElementById('MyWorkSection');
+      //console.log(myWork);
+      myWork.scrollIntoView({ behavior: "smooth" });
+      // var target = $('.section.active').scrollimg('.MyWork');
+      // if (target.length == 0){
+      //   target = $('.MyWork:first');
+      // }
+      // $('html, body').animate({scrollTop: target.offset().top}, 'slow');
+      // $('.active').removeClass('active');
+      // target.addClass('active');
    
     });
+
+    // $('.scrollimg').click(function() {
+    //   var target = $('.section.active').scrollimg('.section');
+    //   if (target.length == 0){
+    //     target = $('.section:first');
+    //   }
+    //   $('html, body').animate({scrollTop: target.offset().top}, 'slow');
+    //   $('.active').removeClass('active');
+    //   target.addClass('active');
+   
+    // });
   });
 
   
